881395633d07a17d65b27f560edfba9798aa5417
ihyup32@gmail.com <jinhyup.lee@bespinglobal.com>
2023-06-22 16:29:31 +0900

    jpa 시작 - ddl, dml 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
-INSERT INTO `class` (`cid`, `className`, `teacher`, `created`, `updated`) VALUES (1, '1-1', '태연', '2023-06-24 22:07:40', '2023-02-24 22:07:40');
-INSERT INTO `class` (`cid`, `className`, `teacher`, `created`, `updated`) VALUES (2, '1-2', '아이유', '2023-06-25 22:13:17', '2023-03-25 22:13:17');
+INSERT INTO `class_room` (`cid`, `class_name`, `teacher`, `created`, `updated`) VALUES (1, '1-1', '태연', '2023-06-24 22:07:40', '2023-02-24 22:07:40');
+INSERT INTO `class_room` (`cid`, `class_name`, `teacher`, `created`, `updated`) VALUES (2, '1-2', '아이유', '2023-06-25 22:13:17', '2023-03-25 22:13:17');
 
-INSERT INTO `student` (`sid`, `cid`, `studentName`) VALUES (1, 1, '브록');
-INSERT INTO `student` (`sid`, `cid`, `studentName`) VALUES (2, 1, '상디');
-INSERT INTO `student` (`sid`, `cid`, `studentName`) VALUES (3, 2, '루피');
-INSERT INTO `student` (`sid`, `cid`, `studentName`) VALUES (4, 2, '나미');
+INSERT INTO `student` (`sid`, `cid`, `student_name`) VALUES (1, 1, '브록');
+INSERT INTO `student` (`sid`, `cid`, `student_name`) VALUES (2, 1, '상디');
+INSERT INTO `student` (`sid`, `cid`, `student_name`) VALUES (3, 2, '루피');
+INSERT INTO `student` (`sid`, `cid`, `student_name`) VALUES (4, 2, '나미');
 
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,17 +1,17 @@
-CREATE TABLE IF NOT EXISTS `class` (
-       `cid` INT(11) NOT NULL AUTO_INCREMENT COMMENT '학생아이디',
-       `className` VARCHAR(20) NULL DEFAULT NULL COMMENT '학급이름',
-       `teacher` VARCHAR(20) NULL DEFAULT NULL COMMENT '선생이름',
-       `created` DATETIME NULL DEFAULT NULL COMMENT '등록일자',
-       `updated` DATETIME NULL DEFAULT NULL COMMENT '수정일자',
-       PRIMARY KEY (`cid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE IF NOT EXISTS `class_room` (
+    `cid` INT(11) NOT NULL AUTO_INCREMENT COMMENT '학생아이디',
+    `class_name` VARCHAR(20) NULL DEFAULT NULL COMMENT '학급이름',
+    `teacher` VARCHAR(20) NULL DEFAULT NULL COMMENT '선생이름',
+    `created` DATETIME NULL DEFAULT NULL COMMENT '등록일자',
+    `updated` DATETIME NULL DEFAULT NULL COMMENT '수정일자',
+    PRIMARY KEY (`cid`)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `student` (
-      `sid` INT(11) NOT NULL AUTO_INCREMENT,
-      `cid` INT(11) NOT NULL DEFAULT 0,
-      `studentName` VARCHAR(20) NULL DEFAULT '' COMMENT '학생이름',
-      PRIMARY KEY (`sid`),
-      INDEX `FK_class_student` (`cid`),
-      CONSTRAINT `FK_class_student` FOREIGN KEY (`cid`) REFERENCES `class` (`cid`) ON UPDATE CASCADE ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+    `sid` INT(11) NOT NULL AUTO_INCREMENT,
+    `cid` INT(11) NOT NULL DEFAULT 0,
+    `student_name` VARCHAR(20) NULL DEFAULT '' COMMENT '학생이름',
+    PRIMARY KEY (`sid`),
+    INDEX `FK_class_room_student` (`cid`),
+    CONSTRAINT `FK_class_room_student` FOREIGN KEY (`cid`) REFERENCES `class_room` (`cid`) ON UPDATE CASCADE ON DELETE CASCADE
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
